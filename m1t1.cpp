62fb66cd3d0033b905dbd3d1acb7253a90a76eb1
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-08-25 15:04:26 +0000

    M1T1, was created and greets the user

---
+++ m1t1.cpp
@@ -0,0 +1,22 @@
+/*
+CSC 134
+Travis Cayton
+8/25/2025
+m1t1 - hello, user
+
+INPUT: run the program
+PROCESS: None yet
+OUTPUT: return - (no error)
+SPECIAL FX: Greet the user.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // SFX: Greet user
+    cout << "hello, user!" << endl;
+
+    // output
+    return 0; // errors
+}
