0283d5f9b240c11908661deacc869e34a177f469
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-09-03 14:18:53 +0000

    M1T2 created

---
+++ M1T2.cpp
@@ -0,0 +1,18 @@
+// CSC 134
+// M1T2 - Hello, Student
+// Travis Cayton
+// 9/3/2025
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string first_name = "Travis ";
+    string last_name = "Cayton";
+    string enroll = "CSC 134";
+
+    cout << first_name << last_name << endl;
+    cout << "I'm enrolled in: " << enroll << endl;
+    cout << "I'm an aspiring game developer" << endl;
+
+}
