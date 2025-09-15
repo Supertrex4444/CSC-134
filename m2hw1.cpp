a48a8f848bb29652b82bf91774a90efe5403579b
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-09-15 14:47:55 +0000

    Created m2hw1

---
+++ m2hw1.cpp
@@ -0,0 +1,32 @@
+/*
+M2HW1
+CSC 134
+Travis Cayton
+9/15/2025
+*/
+
+#include <iostream>
+#include <>
+using namespace std;
+
+void question1(){
+    cout << "Question 1" << endl;
+    
+}
+
+void question2(){
+    cout << "Question 2" << endl;
+    
+}
+
+int main() {
+    question1();
+    question2();
+
+    cout << "Example of printing out correct money values" << endl;
+    double cost = 8.5;
+    cout << setprecision()
+    cout << "The  cost is: $" << cost << endl;
+
+    return 0;
+}
