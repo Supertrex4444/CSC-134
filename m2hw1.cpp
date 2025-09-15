e402ae78e3588eb733a82d9177766bc189eebeb8
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-09-15 15:16:57 +0000

    Updated m2hw1

--- m2hw1.cpp
+++ m2hw1.cpp
@@ -6,17 +6,28 @@ Travis Cayton
 */
 
 #include <iostream>
-#include <>
+#include <iomanip>
 using namespace std;
 
+
 void question1(){
-    cout << "Question 1" << endl;
-    
+    string name;
+    int starting_balance;
+    int deposit;
+    int withdrawal;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Enter your account balance: $";
+    cin >> starting_balance;
+    cout << "Enter the amount you want to deposit: $";
+    cin >> deposit;
+    cout << "Enter the amount you want to withdraw: $";
+    cin >> withdrawal;
+
 }
 
 void question2(){
     cout << "Question 2" << endl;
-    
 }
 
 int main() {
@@ -25,7 +36,7 @@ int main() {
 
     cout << "Example of printing out correct money values" << endl;
     double cost = 8.5;
-    cout << setprecision()
+    //cout << setprecision();
     cout << "The  cost is: $" << cost << endl;
 
     return 0;
