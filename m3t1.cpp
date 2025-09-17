2024f57c5b7f566a7fc77440568f09471bddb6bf
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-09-17 14:48:49 +0000

    Finished m3t1

--- m3t1.cpp
+++ m3t1.cpp
@@ -37,6 +37,18 @@ int main() {
     cout << "The area of the first rectangle is: " << area1 << endl;
     cout << endl;
     cout << "The area of the second rectangle is: " << area2 << endl;
+    cout << endl;
+    if (area1 > area2) {
+        cout << "The first rectangle is larger." << endl;
+    }
+
+    if (area2 > area1) {
+        cout << "The second rectangle is larger." << endl;
+    }
+
+    if (area2 == area1) {
+        cout << "The rectangles are the same area." << endl;
+    }
 
     return 0;
 }
