0207074f4b4a2b909a790377d2245ae277a32e1f
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-09-17 14:32:38 +0000

    Updated m3t1

--- m3t1.cpp
+++ m3t1.cpp
@@ -12,6 +12,31 @@ Travis Cayton
 using namespace std;
 
 int main() {
-    
+    double length1, length2, width1, width2;
+    double area1, area2;
+    cout << "Enter the length and width of two rectangles." << endl;
+    cout << endl;
+    cout << "---First rectangle---" << endl;
+    cout << "Length: ";
+    cin >> length1;
+    cout << "Width: ";
+    cin >> width1;
+    cout << endl;
+    cout << "---Second rectangle---" << endl;
+    cout << "Length: ";
+    cin >> length2;
+    cout << "Width: ";
+    cin >> width2;
+    cout << endl;
+
+    // Calculate the area
+    area1 = length1 * width1;
+    area2 = length2 * width2;
+
+    // Print the output
+    cout << "The area of the first rectangle is: " << area1 << endl;
+    cout << endl;
+    cout << "The area of the second rectangle is: " << area2 << endl;
+
     return 0;
 }
