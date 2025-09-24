5cec451b99c82d05017a25aded32fb7d05c2a528
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-09-24 15:15:03 +0000

    Updated m3lab1

--- m3lab1.cpp
+++ m3lab1.cpp
@@ -10,11 +10,11 @@ using namespace std;
 
 int playerHealth = 100;
 int attackDamage = 1;
-int gold = 20;
+double gold = 20.0;
 
 string createCharacter(){
 string characterName;
-cout << "Welcome to [game name]" << endl;
+cout << "Welcome to Dragon Decide!" << endl;
 cout << endl;
 cout << "Character Name: ";
 cin >> characterName;
@@ -24,15 +24,34 @@ return characterName;
 string characterName = createCharacter();
 
 
-
-
-
 void townCenter(){
-    
+    cout << "\n" << characterName << " arrives at The Town Center" << endl;
+    cout << "1. Go to Merchant's Shop" << endl;
+    cout << "2. Go to Casino" << endl;
+    cout << "3. Travel to Dragon's Lair" << endl;
+    cout << endl;
+    cout << "Choice: ";
+    int choice;
+    cin >> choice;
+    if (choice == 1){
+        merchantShop();
+    }
+        
+    else if (choice == 2){
+        casinoRoom();
+    }
+
+    else if (choice == 3){
+        bossRoom();
+    }
 }
 
 void bossRoom(){
+    cout << "\n" << characterName << " arrives at the Dragon Lair." << endl;
+}
 
+void casinoRoom(){
+    cout << "\n" << characterName << " arrives at the Casino." << endl;
 }
 
 void merchantShop(){
@@ -59,9 +78,8 @@ void merchantShop(){
         
     else if (choice == 2){
         if (gold >= 500){
-            cout << "You purchased the Fiery Greatsword! (gained 50 Attack Damage!)";
-            playerHealth = playerHealth + 50;
-
+            cout << "You purchased the Dragonscale Armor! (gained 100 Health!)";
+            playerHealth = playerHealth + 100;
         }
         else if ((gold < 650)){
             cout << "Merchant: You cant afford Dragonscale Armor!";
