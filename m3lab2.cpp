ebac9d7f9bcec5fb35519b241ac0d525931ac206
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-09-29 14:47:09 +0000

    Completed grade if statements

--- m3lab2.cpp
+++ m3lab2.cpp
@@ -1,5 +1,8 @@
 /*
-
+CSC 134
+M3LAB2
+Travis Cayton
+9/29/2025
 */
 
 #include <iostream>
@@ -21,8 +24,29 @@ void letter_grader(){
     string letter_grade;
     cout << "Enter a number grade 0-100: ";
     cin >> num_grade;
-
+    const double A_GRADE = 90;
+    const double B_GRADE = 80;
+    const double C_GRADE = 70;
+    const double D_GRADE = 60;
     letter_grade = "A";
 
+    if (num_grade >= A_GRADE) {
+        letter_grade = "A";
+    }
+    else if (num_grade >= B_GRADE) {
+        letter_grade = "B";
+    }
+    else if (num_grade >= C_GRADE) {
+        letter_grade = "C";
+    }
+    else if (num_grade >= D_GRADE) {
+        letter_grade = "D";
+    }
+    else {
+        letter_grade = "F";
+    }
+
     cout << "A number of " << num_grade << " is a(n) " << letter_grade;
+    cout << endl;
+    cout << endl;
 }
