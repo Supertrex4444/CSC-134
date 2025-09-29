3ff4fbd8d3c3868c5a6f87f4b0aac5e90f57faad
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-09-29 15:26:36 +0000

    Completed m3lab2

--- m3lab2.cpp
+++ m3lab2.cpp
@@ -13,10 +13,48 @@ using namespace std;
 
 void letter_grader();
 void combat();
+int roll();
 
 int main(){
-    letter_grader();
+    //letter_grader();
+    combat();
     return 0;
+}
+
+
+void combat(){
+    int attack_roll, attack_bonus, enemy_armor;
+    
+    cout << "You are fighting a goblin." << endl;
+    /*
+    cout << "Enter your roll: " << endl;
+    cin >> attack_roll;
+    cout << "Enter attack bonus" << endl;
+    cin >> attack_bonus;
+    cout << "Ememy armor class: " << endl;
+    cin >> enemy_armor;
+    */
+    srand(time(0));
+    attack_roll = roll();
+    attack_bonus = roll();
+    enemy_armor = roll();
+    cout << "Roll: " << attack_roll << " + " << attack_bonus << " = " << attack_roll+attack_bonus << endl;
+    cout << endl;
+    cout << "Enemy Armor: " << enemy_armor << endl;
+    cout << endl;
+    if (attack_roll + attack_bonus >= enemy_armor){
+        cout << "Hit!" << endl;
+    }
+    else {
+        cout << "Miss!" << endl;
+    }
+
+    cout << "Again? (y/n): ";
+    string again;
+    cin >> again;
+    if (again == "y") {
+        combat();
+    }
 }
 
 void letter_grader(){
@@ -49,4 +87,11 @@ void letter_grader(){
     cout << "A number of " << num_grade << " is a(n) " << letter_grade;
     cout << endl;
     cout << endl;
+}
+
+int roll() {
+    const int SIDES = 20;
+    int my_roll;
+    my_roll = (rand() % SIDES) + 1;
+    return my_roll;
 }
