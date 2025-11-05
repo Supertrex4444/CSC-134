d5558f4c2ea9c630157cc3bd1e40baa6c9c2a2b6
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-11-05 15:48:59 +0000

    Added showChoices

--- m5lab1.cpp
+++ m5lab1.cpp
@@ -9,10 +9,12 @@ Travis Cayton
 using namespace std;
 
 int getPlayerChoice(int maxChoice);
+void showChoices(string choice1, string choice2, string choice3);
 
 int main() {
     int choice;
     int max = 3;
+    showChoices("1","2","3");
     cout << "Testing: 1,2, or 3." << endl;
     choice = getPlayerChoice(max);
     cout << "You chose: " << choice << endl;
@@ -37,7 +39,35 @@ int getPlayerChoice(int maxChoice) {
         if (choice >= 1 && choice <= maxChoice) {
             return choice;
         }
+        else {
+            cin.clear();
+            cin.ignore();
+            cout << "Try again" << endl;
+            cout << endl;
+        }
 
         cout << "Please choose between 1 and " << maxChoice << ".\n";
     }
 }
+
+void showChoices(string choice1, string choice2, string choice3) {
+
+    cout << "--- MAKE YOUR CHOICE ---" << endl;
+    int num = 1;
+    cout << num << ". " << choice1 << endl;
+    num++;
+
+    if (choice2 != "") {
+        cout << num << ". " << choice2 << endl;
+        num++;
+    }
+
+        if (choice3 != "") {
+        cout << num << ". " << choice3 << endl;;
+        num++;
+    }
+}
+
+/////////////////////////
+// Story functions
+////////////////////////
