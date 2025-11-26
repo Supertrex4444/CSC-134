6546807180e4712be4d9be0204dba4eba83d0580
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-11-26 15:44:57 +0000

    Updated m6hw1 talent points

--- m6hw1.cpp
+++ m6hw1.cpp
@@ -3,6 +3,7 @@ CSC 134
 m6hw1 - Dungeon Decide
 Travis Cayton
 11/24/25
+Gold
 */
 
 
@@ -19,7 +20,10 @@ int player_health = 100;
 
 void createCharacter();
 void gameStart();
+void screenUpdate();
 int statPoints(int unspentPoints);
+
+
 
 void createCharacter() {
 cout << "-CHARACTER CREATOR-" << endl; 
@@ -27,12 +31,40 @@ cout << endl;
 cout << "Enter character name: ";
 cin >> player_name;
 statPoints(5);
+cout << "Nice! now you have all your talent points";
 }
 
 int statPoints(int unspentPoints){
-    cout << "You have " << unspentPoints << " unspent stat points." << endl << "1 -> Strength(" << player_str << ")" << endl << "2 -> Intelligence(" << player_int << ")" << endl << "3 -> Agility(" << player_agi << ")" << endl << "4 -> Charisma(" << player_char << ")" << endl;
-    cout << endl;
-    
+    while (unspentPoints > 0) {
+        screenUpdate();
+        cout << "You have " << unspentPoints << " unspent stat points." << endl << "1 -> Strength(" << player_str << ")" << endl << "2 -> Intelligence(" << player_int << ")" << endl << "3 -> Agility(" << player_agi << ")" << endl << "4 -> Charisma(" << player_char << ")" << endl;
+        cout << endl;
+        string choice = "";
+        cout << "Enter Choice: ";
+        cin >> choice;
+        while (choice != "1" && choice != "2" && choice != "3" && choice != "4" ) {
+            cout << "TRY AGAIN, Enter Choice: ";
+            cin >> choice;
+        }
+        if (choice == "1") {
+            player_str = player_str + 1;
+            unspentPoints = unspentPoints - 1;
+        }
+        if (choice == "2") {
+            player_int = player_int + 1;
+            unspentPoints = unspentPoints - 1;
+        }
+        if (choice == "3") {
+            player_agi = player_agi + 1;
+            unspentPoints = unspentPoints - 1;
+        }
+        if (choice == "4") {
+            player_char = player_char + 1;
+            unspentPoints = unspentPoints - 1;
+        }
+        
+    }
+    return 0;
 }
 
 void gameStart() {
@@ -46,6 +78,40 @@ void gameStart() {
     createCharacter();
 }
 
+
+void screenUpdate() {
+    cout << R"(
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    )" << endl;
+}
 
 int main() {
     gameStart();
