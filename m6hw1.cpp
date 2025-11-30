61511759620c89c9337a17268c7e65f01d3f6b4c
Travis Cayton <caytont4247@student.faytechcc.edu>
2025-11-30 21:04:35 +0000

    m6hw1 gameplay loop added

--- m6hw1.cpp
+++ m6hw1.cpp
@@ -27,6 +27,7 @@ int enemy_int = 1;
 int enemy_agi = 1;
 int enemy_char = 1;
 int enemy_health = 100;
+int initial_enemy_health = 0;
 string currentRoomName = "";
 
 
@@ -40,6 +41,7 @@ void createRoom();
 void createEnemy();
 void roomGameplay();
 void combat();
+void gameover();
 int statPoints(int unspentPoints);
 int rng(int num);
 
@@ -52,6 +54,7 @@ void createEnemy() {
     enemy_agi = 1 + rng(8);
     enemy_char = 1 + rng(8);
     enemy_health = 25 + rng(75);
+    initial_enemy_health = enemy_health;
     int randomNumber1 = rng(47);
     int randomNumber2 = rng(5);
     enemy_name = name[randomNumber1] + " the " + enemy_type[randomNumber2];
@@ -78,13 +81,42 @@ int rng(int num) {
 
 void roomGameplay() {
     createEnemy();
+    cout << player_name << " encounters " << enemy_name << " in The " << currentRoomName << endl << endl;
     combat();
+    if (player_health > 0) {
+        cout << currentRoomName << " has been cleared!" << endl;
+        characterProfile();
+        cout << "Type '1' to enter the next room" << endl << endl;
+        string choice = "";
+        cin >> choice;
+
+        while (choice != "1") {
+            cout << "TRY AGAIN, Enter Choice: ";
+            cin >> choice;
+        }
+        if (choice == "1") {
+            createRoom();
+        }
+    }
+}
+
+void gameover() {
+    cout << endl << endl;
+    cout << player_name << " had died!" << endl;
+    cout << R"(
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+                                               
+               GAME OVER
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    )" << endl;
 }
 
 void combat() {
     while (enemy_health > 0 && player_health > 0){
         
         int damage_dealt = 0;
+        int enemy_damage = 0;
         cout << player_name << " is fighting " << enemy_name << "!" << endl;
         cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
         cout << "Your Health: " << player_health << " | " << "Enemy's Health: " << enemy_health << endl;
@@ -101,38 +133,61 @@ What will you do?
         cout << "Enter choice: ";
         string choice = "";
         cin >> choice;
-        screenUpdate();
         while (choice != "1" && choice != "2" && choice != "3") {
             cout << "TRY AGAIN, Enter Choice: ";
             cin >> choice;
         }
+        screenUpdate();
         if (choice == "1") {
+            damage_dealt = player_str;
             //if strength is greatest
             if (player_str > player_agi && player_str > player_int && player_str > player_char) {
                 damage_dealt = player_str + rng(8);
                 cout << player_name << " strikes " << enemy_name <<  " powerfully with their weapon, dealing " << damage_dealt << " damage!";
-                enemy_health = enemy_health - damage_dealt;
             }
             //if intelligence is greatest
             if (player_int > player_agi && player_int > player_str && player_int > player_char) {
                 damage_dealt = player_int + rng(6);
                 cout << player_name << " casts a frostbolt at " << enemy_name <<  ", dealing " << damage_dealt << " damage!";
-                enemy_health = enemy_health - damage_dealt;   
             }
             //if agility is greatest
             if (player_agi > player_str && player_agi > player_int && player_agi > player_char) {
                 damage_dealt = player_agi + rng(4);
                 cout << player_name << " preforms a swift attack to " << enemy_name <<  ", dealing " << damage_dealt << " damage!";
-                enemy_health = enemy_health - damage_dealt;
+                
             }
             //if charisma is greatest
             if (player_char > player_str && player_char > player_int && player_char > player_agi) {
                 damage_dealt = player_char + rng(2);
                 cout << player_name << " insults " << enemy_name <<  ", dealing " << damage_dealt << " emotional damage!";
-                enemy_health = enemy_health - damage_dealt;
+                
+            }
+            enemy_health = enemy_health - damage_dealt;
+        }
+        cout << endl << endl;
+
+        if (enemy_health > 0) {
+            enemy_damage = 5 + rng(5);
+            cout << enemy_name << " deals " << enemy_damage << " damage to you!";
+            player_health = player_health - enemy_damage;
+        }
+        
+        else{
+            screenUpdate();
+            cout << enemy_name << " has been defeated!" << endl;
+            player_xp = player_xp + initial_enemy_health;
+            cout << "You gained " << player_xp << " XP!" << endl;
+            if (player_xp >= 100) {
+                player_level = player_level + 1;
+                player_health = player_health + 30;
+                cout << player_name << " has reached level " << player_level << "!" << endl << endl;
+                statPoints(2);
+                player_xp = 0;
             }
         }
-        cout << endl << endl << endl << endl;
+        
+        
+        cout << endl << endl;
     }
 }
 
@@ -142,18 +197,34 @@ void createCharacter() {
     cout << endl;
     cout << "Enter character name: ";
     cin >> player_name;
+    screenUpdate();
     statPoints(5);
+    screenUpdate();
     characterProfile();
-    cout << "|-Press any key to begin-|" << endl;
-    cout << "The game started" << endl;
-    createRoom();
+    cout << "Type '1' to start game" << endl << endl;
+    string choice = "";
+    cin >> choice;
+
+    while (choice != "1") {
+        cout << "TRY AGAIN, Enter Choice: ";
+        cin >> choice;
+    }
+    if (choice == "1") {
+        cout << "The game has started" << endl;
+        while (player_health > 0){
+            createRoom();
+        }
+        if (player_health <= 0) {
+            gameover();
+        }
+    }
 }
 
 void characterProfile(){
-    screenUpdate();
     cout << "             Character Profile" << endl;
     cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
     cout << "Name: " << player_name << endl << "Strength: (" << player_str << ")" << endl << "Intelligence: (" << player_int << ")" << endl << "Agility: (" << player_agi << ")" << endl << "Charisma: (" << player_char << ")" << endl;
+    cout << "Level: " << player_level << endl << "XP: " << player_xp << "/100" << endl;
     cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
     cout << endl;
     cout << endl;
@@ -161,7 +232,6 @@ void characterProfile(){
 
 int statPoints(int unspentPoints){
     while (unspentPoints > 0) {
-        screenUpdate();
         cout << "You have " << unspentPoints << " unspent stat points." << endl << "1 -> Strength(" << player_str << ")" << endl << "2 -> Intelligence(" << player_int << ")" << endl << "3 -> Agility(" << player_agi << ")" << endl << "4 -> Charisma(" << player_char << ")" << endl;
         cout << endl;
         string choice = "";
@@ -187,7 +257,7 @@ int statPoints(int unspentPoints){
             player_char = player_char + 1;
             unspentPoints = unspentPoints - 1;
         }
-        
+        screenUpdate();
     }
     return 0;
 }
